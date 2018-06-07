c6b9acd4f0ffbdda0fb4b9af12f2b430877a3c75
vafinalex@gmail.com <vafinalex@gmail.com>
2018-06-07 14:42:39 +0300

    Exeption catch
    Catch exeption when user insert letters in price

--- src/CurrencyCount.java
+++ src/CurrencyCount.java
@@ -6,17 +6,29 @@ public class CurrencyCount {
 	public static void main(String[] args) {
 		for(int i = 0; ; i++) {
 			Scanner scan = new Scanner(System.in);
+			double price = 0;
 			System.out.println("Insert currency name (UAH, EUR or GBP)");
 			String currencyName = scan.nextLine();
 			currencyName = currencyName.toUpperCase();
 			
 			
 			if (currencyName.equals("UAH") || currencyName.equals("GBP") || currencyName.equals("EUR")){
-				System.out.println("Insert price");
-				double price = scan.nextDouble();
+				
+				for(int j = 0; ;j++) {
+					System.out.println("Insert price");
+					try {
+						price = scan.nextDouble();
+						break;
+					}catch(java.util.InputMismatchException e) {
+						System.out.println("Exeption " + e);
+						System.out.println("Only numbers availible here");
+						break;
+					}
+					
+				}
 				
 				if(price <= 0) {
-					System.out.println("Price should be more than 0");
+					System.out.println("Price should be more than 0 and contains only numbers");
 				}else {
 					System.out.println("");
 					getApi ob = new getApi(currencyName);
